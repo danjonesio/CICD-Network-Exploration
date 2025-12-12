e5d27374c18b2ac25a9637e3dae106a950c6ce01
danjonesio <web@dansjones.uk>
2025-12-12 23:08:29 +0000

    backend.tf changes to null out AWS account details requested

--- backend.tf
+++ backend.tf
@@ -2,10 +2,11 @@ terraform {
   backend "s3" {
     key    = "network-cicd/terraform.tfstate"
     region = "us-east-1"
-
+    
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
-    force_path_style            = true
+    skip_requesting_account_id  = true
+    use_path_style              = true
   }
 }
