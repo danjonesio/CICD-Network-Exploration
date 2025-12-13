331f776b2aae5e32dd0f80c5168a88e287b8adaa
danjonesio <web@dansjones.uk>
2025-12-13 16:16:27 +0000

    testing delete mode.

--- modules/banner/banner.tf
+++ modules/banner/banner.tf
@@ -4,4 +4,5 @@ resource "iosxe_banner" "banner_lab" {
   device = each.key
 
   login_banner = "** This is a lab device managed by Terraform. Unauthorized access is prohibited. **"
+  delete_mode = "all"
 }
