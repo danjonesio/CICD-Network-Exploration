f9392e7aba13410d4f9a98afa75e8eb849f0b6f5
danjonesio <web@dansjones.uk>
2025-12-13 15:28:15 +0000

    creation of destroy_example script for TF destroy manual runs.

---
+++ destroy_example.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# This script destroys the Terraform-managed infrastructure and saves the configurations on the network devices.
+# set your S3 credentials and Terraform variables
+
+export AWS_ACCESS_KEY_ID="S3_ACCESS_KEY_ID"
+export AWS_SECRET_ACCESS_KEY="S3_SECRET_ACCESS"
+export TF_VAR_username="admin"
+export TF_VAR_password="cisco"
+
+# set your S3 endpoint and bucket name
+terraform init -backend-config="endpoints={s3=\"S3_ENDPOINTURL\"}" -backend-config="bucket=ARTIFACT_BUCKET_NAME"
+terraform destroy
+
+# Save configs after destroy
+echo "Saving configurations to startup-config..."
+for host in 192.168.2.220 192.168.2.221 192.168.2.222 192.168.2.223; do
+  echo "Saving config on $host..."
+  sshpass -p "cisco" ssh -o StrictHostKeyChecking=no admin@$host "write mem"
+done
+
+echo "All configurations saved!"
--- modules/old.versionstf
+++
@@ -1,8 +0,0 @@
-terraform {
-  required_providers {
-    iosxe = {
-      source  = "CiscoDevNet/iosxe"
-      version = "0.13.0"
-    }
-  }
-}
