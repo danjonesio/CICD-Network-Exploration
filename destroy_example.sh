4d9193db26a0fce7b838393badc73875a93d9371
danjonesio <web@dansjones.uk>
2025-12-14 08:59:01 +0000

    interfaces module setup.

--- destroy_example.sh
+++ destroy_example.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 # This script destroys the Terraform-managed infrastructure and saves the configurations on the network devices.
 # set your S3 credentials and Terraform variables
+# set S3 URL if using a custom endpoint/ S3 compatible storage, otherwise remove it.
 
 export AWS_ACCESS_KEY_ID="S3_ACCESS_KEY_ID"
 export AWS_SECRET_ACCESS_KEY="S3_SECRET_ACCESS"
 export TF_VAR_username="admin"
 export TF_VAR_password="cisco"
+export AWS_ENDPOINT_URL_S3="S3_ENDPOINTURL"
 
 # set your S3 endpoint and bucket name
-terraform init -backend-config="endpoints={s3=\"S3_ENDPOINTURL\"}"
+terraform init
 terraform destroy
 
 # Save configs after destroy
---
+++ modules/int_template/interface_template.tf
@@ -0,0 +1,3 @@
+resource "iosxe_template" "core_link_template" {
+  template_name = "Core-Link-Template_WAN"
+}
